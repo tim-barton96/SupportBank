9a6c0b247a5650b162149a3a911ecc9329e262d8
Timothy Edward Barton <tbart@xwt34pl7nl.uk.aexp.com>
2022-09-14 12:20:45 +0100

    Attempt 2

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -3,6 +3,6 @@ package training.supportbank;
 public class Main {
     public static void main(String args[]) {
         // Your code here!
-        System.out.println("Test!");
+        System.out.println("Test baby!");
     }
 }
